0ae5ea76c7c69f02ef5aa2c06c5fad6ae604b886
Oneballnote <oneballnotita@gmail.com>
2024-03-14 21:11:17 -0600

    Ya quedo esta mamada

--- Practica1.asm
+++ Practica1.asm
@@ -7,41 +7,43 @@
 #}
 
 .text
-	addi s0, zero, 1 #s0 = 3 numero de discos
+	addi s0, zero, 3 #s0 = 3 numero de discos
 	#declarar torres
-	lui s1, 0x10001 #torre 1
-	slli t0, s0, 2 	#calculamos el espacio de cada arreglo basado en los discos
-	add s2, s1, t0 #torre 2
-	add s3, s2, t0 #torre 3
+	lui s1, 0x10001 #asignamos la torre origen
+	slli t0, s0, 2 #calculamos el espacio de cada arreglo basado en los discos
+	add s2, s1, t0 #asignamos la torre auxiliar
+	add s3, s2, t0 #asignamos la torre destino
 	
-	add t0, zero, s0
+	add a2, a2, s0 #Guardamos n para no perder el apuntado original
+	add a3, s1, zero #Guardamos el puntero de la torre original 
+	#Detalle importante, es necesario settear los siguiente punteros 
+	#una posicion después del inicio de la torre, asi que volvemos a sumar al puntero inicial
+	add a4, s2, t0 #auxiliar
+	add a5, s3, t0 #destino
+	
+	addi t0, zero, 1
 	add t1, t1, s1
-for:	#llenamos la torre inicial con los numeros 1-s0
-	bge zero, t0 endfor 
+for:	#llenamos la torre origen con los numeros 1-s0
+	blt s0, t0 endfor 
 	#guardamos el numero en la posicion actual
 	sw t0, 0(t1)
 	addi t1, t1, 4 #movemos el apuntador a la siguiente posicion
-	
-	addi t0, t0, -1 #actualizamos t0
+	addi t0, t0, 1 #actualizamos t0
 	
 	jal for
 		
 endfor:
-	
 	#llamamos a la funcion
-	add a2, a2, s0 #n
-	add a3, a3, s1 #origen
-	add a4, a4, s2 #auxiliar
-	add a5, a5, s3 #destino
 	jal hanoi
+	#Terminamos el programa
 	jal endcode
 	
 	
-#void hanoi(int n, int origen, int destino, int auxiliar) {
+#void hanoi(int n, int origen, int auxiliar, int destino) {
 #  if (n == 1) {
 #    printf("Mover disco 1 de %d a %d\n", origen, destino);
 #  } else {
-#    hanoi(n - 1, origen, auxiliar, destino);
+#    hanoi(n - 1, origen, destino, auxiliar);
 #    printf("Mover disco %d de %d a %d\n", n, origen, destino);
 #    hanoi(n - 1, auxiliar, destino, origen);
 #  }
@@ -50,14 +52,79 @@ endfor:
 hanoi:
 	addi t0, zero, 1 
 	bne a2, t0, else  #if a2 != 1 saltamos a else
-	lw t1, 0(a3) #leemos el valor almacenado en la torre de origen
-	sw zero, 0(a3) 	#sobre escribimos el valor con 0
-	sw t1 0(a5) #lo cambiamos a la siguiente torre
-	jalr ra
+	#Ojo con el movimiento
+	sw zero, 0(a3)
+	addi a3, a3, 4
+	addi a5, a5, -4
+	sw a2, 0(a5) 
+	#jal t2, move	
+	jalr ra 
 else:
 	#mas de dos discos
+	#hanoi(n - 1, origen, destino, auxiliar);
+	
+	addi sp, sp, -4 #hacemos espacio en el stack para guardar ra
+	sw ra, 0(sp)
+	addi sp, sp, -4 ##hacemos espacio en el stack para guardar n
+	sw a2, 0(sp)
+	
+	addi a2, a2, -1
+	
+	add t0, a4, zero #t0 = auxiliar
+	add a4, a5, zero #a4 = destino
+	add a5, t0, zero #a5 = auxiliar
+	
+	jal hanoi
+	
+	add t0, a4, zero #t0 = destino
+	add a4, a5, zero #a4 = auxiliar
+	add a5, t0, zero #a5 = destino
+	
+	#origen
+	lw a2, 0(sp)
+	addi sp, sp, 4
+	lw ra, 0(sp)
+	addi sp, sp, 4
+	
+	#mov
+	sw zero, 0(a3)
+	addi a3, a3, 4
+	addi a5, a5, -4
+	sw a2, 0(a5)
+		
+	#cambios de discos
+	#hanoi(n - 1, auxiliar, destino, origen);
+	#jal t2, move
+	addi sp, sp, -4 #hacemos espacio en el stack para guardar ra
+	sw ra, 0(sp)
+	addi sp, sp, -4 ##hacemos espacio en el stack para guardar n
+	sw a2, 0(sp)
+	
+	addi a2, a2, -1
+
+	add t0, a3, zero #t0 = origen
+	add a3, a4, zero #a3 = auxiliar
+	add a4, t0, zero #a4 = origen
+	
+	jal hanoi
+	
+	add t0, a3, zero #temp = auxiliar
+	add a3, a4, zero #a3 = origen
+	add a4, t0, zero #a5 = auxiliar
+	
+	#origen
+	lw a2, 0(sp)
+	addi sp, sp, 4
+	lw ra, 0(sp)
+	addi sp, sp, 4
 	
 	jalr ra
 
+#move:
+#	sw zero, 0(a3) 
+#	addi a3, a3, 4
+#	addi a5, a5, -4
+#	sw a2, 0(a5)
 endcode:nop
+
 
